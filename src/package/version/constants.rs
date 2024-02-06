8f2fed94444e5ebbb523dd96c738994b054b5086
Shahen Hovhannisyan <shahen.hovhannisyan.94@gmail.com>
2024-02-06 16:50:38 +0400

    fix(version): Enhanced version parsing

--- src/package/version/constants.rs
+++ src/package/version/constants.rs
@@ -1,5 +1,5 @@
 // To match ^1.50.5-alpha.123+build123 like version strings
-pub const SEMVER_REGEX: &str = r"^(?P<operator>\^|~|=)?\s?(?P<major>\d+|x|\*)(?:\.(?P<minor>\d+|x|\*))?(?:\.(?P<patch>\d+|x|\*))?(?:[-.](?P<alpha>[a-zA-Z0-9-]+(?:\.\w+)?))?(?:\+(?P<build>[a-zA-Z0-9-+]+))?$";
+pub const SEMVER_REGEX: &str = r"^(?P<operator>\^|~|=)?\s?(?P<major>\d+|x|\*)(?:\.(?P<minor>\d+|x|\*))?(?:\.(?P<patch>\d+|x|\*))?(?:[-.](?P<alpha>[^+]+))?(?:\+(?P<build>[a-zA-Z0-9-+]+))?$";
 
 // To match >=1.50.5 <2.0.0 like version strings
 pub const RANGE_REGEX: &str = r"^(\s+)?(?P<start_operator>~?[<>]=?|~|\^)?\s?(?P<start_major>\d+|x|\*)(?:\.(?P<start_minor>\d+|x|\*))?(?:\.(?P<start_patch>\d+|x|\*))?(?:(?P<connector>,|\|\|)?\s*(?P<end_operator>[<>]=?|~|\^)?\s*(?P<end_major>\d+|x|\*)(?:\.(?P<end_minor>\d+|x|\*))?(?:\.(?P<end_patch>\d+|x|\*))?)?$";
